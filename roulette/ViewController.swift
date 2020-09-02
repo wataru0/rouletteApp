ace2a571e077b437292cd860e6af166caf423765
wataru0 <wataru.tv.nanao@gmail.com>
2020-09-02 14:46:55 +0900

    first commit

---
+++ roulette/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  roulette
+//
+//  Created by 岡本航昇 on 2020/09/02.
+//  Copyright © 2020 wataru okamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
